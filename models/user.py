e992b6c96c17d75998bba9882588cdd37cad7164
gabbrenn <gabvladimirbrenn@gmail.com>
2024-01-16 15:48:59 -0800

    models

---
+++ models/user.py
@@ -0,0 +1,13 @@
+user.py
+#!/usr/bin/python3
+"""This module creates a User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Class for managing user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
